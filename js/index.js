b0a4020e8813206fb35debbd83342ac9531d73f9
Dawid <dawidkwasowski@gmail.com>
2021-12-15 21:31:25 +0100

    smallfixes

--- js/index.js
+++ js/index.js
@@ -3,18 +3,22 @@ import * as THREE from '../node_modules/three/build/three.module.js'
 import { OrbitControls } from './vendor_mods/three/examples/js/controls/OrbitControls.js'
 
 const scene = new THREE.Scene();
+
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+const light = new THREE.AmbientLight( 0x000000,.3);
+scene.add( light );
+light.position.set( 0, 2, 2 );
 const createBorder = (width,depth = 0.10) => {
   const geometry = new THREE.BoxGeometry(width,0.10,depth);
-  const material = new THREE.MeshBasicMaterial( { color: 0xffff00 } );
+  const material = new THREE.MeshPhongMaterial( { color: 0xffff00,emissive: 0xff00ff,shininess: 0 } );
   const border = new THREE.Mesh( geometry, material );
   
   return border
 }
 
-const makeFill = (width,depth,height) => {
-  const geometry = new THREE.BoxGeometry(width,0.5,depth);
-  const material = new THREE.MeshBasicMaterial( { color: 0x0000ff } );
+const makeFill = (width,depth) => {
+  const geometry = new THREE.BoxGeometry(width,0.2,depth);
+  const material = new THREE.MeshPhongMaterial( { color: 0x0000ff,emissive: 0x000fff,shininess: 100 } );
   const fill = new THREE.Mesh( geometry, material );
   return fill
 }
@@ -43,13 +47,14 @@ const fill = makeFill(3,3,3)
 const frame = makeFrame()
 frame.position.z = -1.5
 shelf.add(frame,fill)
+shelf.position.setY(3)
 scene.add( shelf );
 
 
 camera.position.z = 5;
 const renderer = new THREE.WebGLRenderer();
 const controls = new OrbitControls( camera, renderer.domElement );
-
+renderer.setClearColor( 0xffffff, 1);
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
